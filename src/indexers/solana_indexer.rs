0aac1972618a2d0bbb5a3a88cbba2e1a0e40d197
srajawat-hub <srajawat5868@gmail.com>
2025-03-28 16:14:08 -0300

    fix type

--- src/indexers/solana_indexer.rs
+++ src/indexers/solana_indexer.rs
@@ -410,6 +410,7 @@ impl SolanaIndexer {
                             let status = DepositStatus::Initialized as i32;
 
                             let amount = amount as i64;
+                            let chain_id = self.chain_id.to_string();
 
                             let query = "INSERT INTO deposit_received VALUES(DEFAULT, $1, $2, $3, $4, $5, $6, $7, $8)";
                             let response = database_client
@@ -418,10 +419,10 @@ impl SolanaIndexer {
                                     &[
                                         &user_address,
                                         &token_address,
-                                        &self.chain_id,
+                                        &chain_id,
                                         &amount,
                                         &timestamp,
-                                        &transaction_hash.to_string(),
+                                        &transaction_hash,
                                         &message_id,
                                         &status,
                                     ],
