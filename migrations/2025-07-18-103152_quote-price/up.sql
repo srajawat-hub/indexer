52d486c0b1a0e17cddeff6ba6259eb1321675c8c
srajawat-hub <srajawat5868@gmail.com>
2025-07-20 15:27:41 +0530

    Hookexe abi update (#116)
    * updating hook executor abi
    
    * update hook executor abi

--- migrations/2025-07-18-103152_quote-price/up.sql
+++ migrations/2025-07-18-103152_quote-price/up.sql
@@ -1,7 +1,7 @@
 -- Your SQL goes here
 CREATE TABLE intent_quotes (
     id BIGSERIAL PRIMARY KEY,
-    intent_id BIGINT NOT NULL,
+    intent_id BIGINT NOT NULL UNIQUE,
     quotes JSONB NOT NULL,
     "timestamp" timestamp without time zone NOT NULL 
 )
